c0f14b047c581ec5c501fb8674c85de229f542fa
anna-guinet <24371964+anna-guinet@users.noreply.github.com>
2024-07-05 17:03:11 +0200

    First push website files

---
+++ publications.php
@@ -0,0 +1,35 @@
+<?php require './header.php'; ?>
+
+	<div class="content pure-u-1 pure-u-md-3-4">
+		<div>
+			<!-- A wrapper for all the blog posts -->
+			<div class="posts">
+				<h1 class="content-subhead">NEW PUBLICATION</h1>
+
+				<section class="post">
+					<div class="post-description">
+						<span class="post-dates">March 2021</span>
+						<span class="post-separator">&#9642;</span>
+						<span class="post-venue">Eurocrypt 2021</span>
+						<span class="post-separator">&#9642;</span>
+						<span class="post-title">Ciminion: Symmetric Encryption Based on Toffoli-Gates over Large Finite Fields</a></span>
+						<br />
+						<span class="post-location">with Christoph Dobraunig, Lorenzo Grassi, and Daniël Kuijsters</span>
+						<br />
+						<a class="pure-button post-link post-link-url" href="https://doi.org/10.1007/978-3-030-77886-6_1">Conference Paper</a>
+						<a class="pure-button post-link post-link-url" href="https://eprint.iacr.org/2021/267">Pre-print (Extended Version)</a>
+						<a class="pure-button post-link post-link-pdf" href="https://dblp.org/rec/conf/eurocrypt/Dobraunig0GK21.html?view=bibtex">Bibtex</a>
+						<a class="pure-button post-link post-link-video" href="https://youtu.be/YeaRigKun4Y">Video</a>
+					</div>
+					
+					
+                </section>
+            </div>
+			
+			<?php require 'footer.php'; ?>
+			
+		</div>
+	</div>
+</div>
+</body>
+</html>
